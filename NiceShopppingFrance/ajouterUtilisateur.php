e004e600af60f162d5d0e31151bde0ed09e4aabc
Lamine BA <lemzoba@gmail.com>
2016-05-26 17:02:27 +0200

    Modification du fichier ajouterUtilisateur.php pour rajouter dans le formulaire l'attribut "placeholder="Exemple" pour permettre à l'utilisateur de savoir quoi saisir dans les champs du formulaire.

--- NiceShopppingFrance/ajouterUtilisateur.php
+++ NiceShopppingFrance/ajouterUtilisateur.php
@@ -8,30 +8,30 @@
 
     <body>
     	<h1 style="text-align: center;">Ajouter un employé sur le Système d'information de Nice - Shopping France</h1>
-    	<form method="post" action="addUser/traitement.php">
+    	<form method="post" action="connexion/traitement.php">
  
 		   <fieldset>
 		       	<legend id= "titre_legende">Votre identité</legend> <!-- Titre du fieldset --> 
 		       	
 		       	<!-- Choix de la civilité -->
-		       	<select name="civilite" autofocus="autofocus" autofocus="autofocus" required="requierd">
-		       		<option selected disabled>Votre civilité </option>
+		       	<select name="civilite" autofocus="autofocus" required="requierd">
+		       		<option selected disabled>--- Civilité ---</option>
 		       		<option value="madame">Madame</option>
 		       		<option value="monsieur">Monsieur</option>
 		       		<option value="mademoiselle">Mademoiselle</option>
 		       	</select>
 
 		       	<label for="nom">Votre nom : </label>
-		       	<input type="text" name="nom" id="nom" autofocus="autofocus" required="requierd" value="DUPONT" /><br/>
+		       	<input type="text" name="nom" id="nom" autofocus="autofocus" required="requierd" placeholder="BA" /><br/>
 
 		       	<label for="prenom">Votre prenom : </label>
-		       	<input type="text" name="prenom" id="prenom" autofocus="autofocus" required="requierd" value="Pierre" /><br/>
+		       	<input type="text" name="prenom" id="prenom" autofocus="autofocus" required="requierd" placeholder="Lamine" /><br/>
 
 		       	<label for="dateNaissance">Votre date de naissance : </label>
-		       	<input type="date" name="dateNaissance" id="dateNaissance" autofocus="autofocus" required="requierd"/><br/>
+		       	<input type="date" name="dateNaissance" id="dateNaissance" autofocus="autofocus" required="requierd" placeholder="28/12/1988" /><br/>
 
 		       	<label for="numeroTelephone">Votre numéro de téléphone : </label>
-		       	<input type="tel" name="numeroTelephone" id="numeroTelephone" autofocus="autofocus" required="requierd" value="+33 6 51 58 75 08" /><br/>
+		       	<input type="tel" name="numeroTelephone" id="numeroTelephone" autofocus="autofocus" required="requierd" placeholder="+33 6 51 58 75 08" /><br/>
 		   	</fieldset>
 
 		   	<!-- Adresse de résidence -->
@@ -39,22 +39,22 @@
 		       	<legend id= "titre_legende">Votre adresse de résidence </legend> <!-- Titre du fieldset --> 
 
 		       	<label for="numeroNomVoie">Numero et nom de la voie : </label>
-		       	<input type="text" name="numeroNomVoie" id="numeroNomVoie" autofocus="autofocus" required="requierd" value="1 Avenue Charles de Gaulle" /><br/>
+		       	<input type="text" name="numeroNomVoie" id="numeroNomVoie" autofocus="autofocus" required="requierd" placeholder="1 Avenue Charles de Gaulle" /><br/>
 
 		       	<label for="complementAdresse">Complément adresse : </label>
-		       	<input type="text" name="complementAdresse" id="complementAdresse" autofocus="autofocus" value="Ex : Chez M. DUPONT, Appart 46" /><br/>
+		       	<input type="text" name="complementAdresse" id="complementAdresse" autofocus="autofocus" placeholder="Ex : Chez M. DUPONT, Appart 46" /><br/>
 
 		       	<label for="codePostale">Code Postale : </label>
-		       	<input type="int" name="codePostale" id="codePostale" autofocus="autofocus" required="requierd" value="75010" /><br/>
+		       	<input type="int" name="codePostale" id="codePostale" autofocus="autofocus" required="requierd" placeholder="75010" /><br/>
 
 		       	<label for="ville">Ville : </label>
-		       	<input type="text" name="ville" id="ville" autofocus="autofocus" required="requierd"  value="Paris" /><br/>
+		       	<input type="text" name="ville" id="ville" autofocus="autofocus" required="requierd"  placeholder="Paris" /><br/>
 
 		       	<label for="region">Region : </label>
-		       	<input type="text" name="region" id="region" autofocus="autofocus" value="Ile de France" /><br/>
+		       	<input type="text" name="region" id="region" autofocus="autofocus" placeholder="Ile de France" /><br/>
 
 		       	<label for="pays">Pays : </label>
-		       	<input type="text" name="pays" id="pays" autofocus="autofocus" required="requierd" value="France" /><br/>
+		       	<input type="text" name="pays" id="pays" autofocus="autofocus" required="requierd" placeholder="France" /><br/>
 		   	</fieldset>
 
 		   	<!-- Information de connexion -->
@@ -62,10 +62,10 @@
 		       	<legend id= "titre_legende">Vos informations de connexion </legend> <!-- Titre du fieldset --> 
 		     
 		       	<label for="emailFirst">saisir une adresse mail</label>
-		       	<input type="email" name="emailFirst" id="emailFirst" autofocus="autofocus" required="requierd"/><br/>
+		       	<input type="email" name="emailFirst" id="emailFirst" autofocus="autofocus" required="requierd" placeholder="lamine.ba@niceshopping.fr" /><br/>
 		       	
 		       	<label for="emailSecond">Confirmer votre adresse mail</label>
-		       	<input type="email" name="emailSecond" id="emailSecond" autofocus="autofocus" required="requierd"/><br/>
+		       	<input type="email" name="emailSecond" id="emailSecond" autofocus="autofocus" required="requierd" placeholder="lamine.ba@niceshopping.fr"/><br/>
 
 		       	<label for="passwordFirst">Saisir un mot de passe</label>
 		       	<input type="password" name="passwordFirst" id="passwordFirst" autofocus="autofocus" required="requierd"/><br/>
@@ -76,10 +76,10 @@
 
 		   	<!-- Votre Role -->
 		   	<fieldset>
-		       	<legend id= "titre_legende">Le role de l'utilisateur </legend> <!-- Titre du fieldset --> 
+		       	<legend id= "titre_legende">Le rôle de l'utilisateur </legend> <!-- Titre du fieldset --> 
 		     	<!-- Choix du role de l'utilisateur -->
 		       	<select name="role" autofocus="autofocus" required="requierd">
-		       		<option selected disabled>Role</option>
+		       		<option selected disabled>--- Rôle ---</option>
 		       		<option value="administrateur">Administrateur</option>
 		       		<option value="superUtilisateur">Super Utilisateur</option>
 		       		<option value="directeurGeneral">Directeur général</option>
